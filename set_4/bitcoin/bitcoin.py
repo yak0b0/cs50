dd5660ef1a91101815661b99473bcbe6c3456a24
Jakub Zoldak <s104627@pollub.edu.pl>
2025-08-11 17:16:00 +0200

    Lecture_4 with all problems solved

---
+++ set_4/bitcoin/bitcoin.py
@@ -0,0 +1,37 @@
+import sys
+import requests
+import json
+
+
+def main():
+    number_of_btc = check_command()
+    get_btc_price(number_of_btc)
+
+
+def check_command():
+    try:
+        if len(sys.argv) != 2:
+            raise SystemError
+        if float(sys.argv[1]):
+            number = float(sys.argv[1])
+        return number
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+    except SystemError:
+        sys.exit("Missing command-line argument")
+
+
+def get_btc_price(btc_amount):
+    try:
+        response = requests.get(
+            "https://rest.coincap.io/v3/assets/bitcoin?apiKey=52c956a3ec42dcc6316ec6edf43130d8d9941fde6ef90023aa2fe6e55f061769"
+        )
+        response_json = response.json()
+        btc_price_usd = response_json['data']['priceUsd']
+        final_price = float(btc_price_usd) * btc_amount
+        print(f"${final_price:,.4f}")
+    except requests.RequestException:
+        sys.exit("Request Errir")
+
+
+main()
